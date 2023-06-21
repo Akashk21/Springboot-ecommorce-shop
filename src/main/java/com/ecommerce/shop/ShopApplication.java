1e8bb33fbad7c7a2c5ccaec55b29df3975132790
Akashk21 <pes1ug20cs027@pesu.pes.edu>
2023-06-21 14:12:13 +0530

    first commit

---
+++ src/main/java/com/ecommerce/shop/ShopApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+
+		// System.out.println("Hello, World!");
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecommerce/shop/controller/ShopController.java
@@ -0,0 +1,34 @@
+// // package com.ecommerce.shop.controller;
+// // import org.springframework.stereotype.Controller;
+// // import org.springframework.ui.Model;
+// // import org.springframework.web.bind.annotation.GetMapping;
+
+// // @Controller
+// // public class ShopController {
+
+// //     @GetMapping("/home")
+// //     public String listStudents(Model model) {
+// //         model.addAttribute("home","home");
+// //         return "home";
+// //     }
+
+// // }
+
+// package com.ecommerce.shop.controller;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class ShopController {
+
+//     @GetMapping("/home")
+//     public String showHomePage(Model model) {
+//         model.addAttribute("Home");
+//         // Add any additional attributes required for the home.html page
+//         // For example:
+//         // model.addAttribute("message", "Welcome to the home page!");
+//         return "home";
+//     }
+// }
